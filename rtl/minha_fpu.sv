9a904708246fc527d60986c229486dbb7659710b
Natan Duarte de Oliveira <natanduarteoliveira@gmail.com>
2025-06-13 09:15:38 -0300

    Add files via upload

--- rtl/minha_fpu.sv
+++ rtl/minha_fpu.sv
@@ -15,7 +15,6 @@ module minha_fpu
     // [31]: Sinal | [30:21]: Expoente | [20:0]: Mantissa
     input logic [31:0] op_B_in,
     
-    output logic flags_out,
     output logic [3:0] status_out,
     output logic [31:0] data_out,
 );
@@ -24,24 +23,45 @@ module minha_fpu
     {
         ESPERA    = 4'b0000,
         EXACT     = 4'b0001,
-        OVERFLOW  = 4'b0011, 
-        UNDERFLOW = 4'b0111,
-        INEXACT   = 4'b1111
+        OVERFLOW  = 4'b0010, 
+        UNDERFLOW = 4'b0100,
+        INEXACT   = 4'b1000
     } state_status_out;
     
+    	// FSM
+    typedef enum [2:0]
+    { 
+        EXTRAI    = 3'b000,
+        ALINHA    = 3'b001,
+        OPERACAO  = 3'b010,
+        ARREDONDA = 3'b011,
+        VERIFICA  = 3'b100,
+        OUTPUT    = 3'b101        
+    } state_fsm;
+    
         // Sinais Internos
+    state_fsm state;
     state_status_out status;
-    logic [9:0]  expoente_op_A;
-    logic [9:0]  expoente_op_B;
+    logic sinal_op_A;
+    logic sinal_op_B;
+    logic sinal_data_out;
+    logic [9:0] expoente_op_A;
+    logic [9:0] expoente_op_B;
+    logic [9:0] expoente_data_out;
+    logic [9:0] diferenca_expoente;
     logic [20:0] mantissa_op_A;
     logic [20:0] mantissa_op_B;
+    logic [20:0] mantissa_data_out;
     
         // Atribuições
     assign status_out = status;
     
-    always_ff @(posedge clock_100KHz or posedge reset) begin
+    always_ff @(posedge clock_100KHz or negedge reset) begin
         if (!reset) begin
+            state         <= EXTRAI;
             status        <= ESPERA;
+            sinal_op_A    <= 1'b0;
+            sinal_op_B    <= 1'b0;
             expoente_op_A <= 10'b0;
             expoente_op_B <= 10'b0;
             mantissa_op_A <= 21'b0;
@@ -49,7 +69,63 @@ module minha_fpu
             data_out      <= 31'b0;
         end
         else begin
+            case (state)
             
+                    // ARRUMAR
+                EXTRAI: begin
+                    if (op_A_in != 32'b0 && op_B_in != 32'b0) begin
+                        sinal_op_A    <= op_A_in[31];
+                        expoente_op_A <= op_A_in[30:21];
+                        mantissa_op_A <= op_A_in[20:0];
+                        sinal_op_B    <= op_B_in[31];
+                        expoente_op_B <= op_B_in[30:21];
+                        mantissa_op_B <= op_B_in[20:0];
+                        state <= ALINHA;
+                    end
+                end
+                
+                ALINHA: begin
+                    if (expoente_op_A == expoente_op_B) begin
+                        state <= OPERACAO;
+                    end
+                    else begin
+                        if (expoente_op_A > expoente_op_B) begin
+                            diferenca_expoente <= expoente_op_A - expoente_op_B;
+                            mantissa_op_A  << 
+                        end
+                        else begin
+                            diferenca_expoente <= expoente_op_B - expoente_op_A;
+                        end
+                    end
+                end
+                
+                OPERACAO: begin
+                    if (sinal_op_A == sinal_op_B) begin
+                        sinal_data_out <= sinal_op_A;
+                        mantissa_data_out <= mantissa_op_A + mantissa_op_B;
+                    end
+                    else begin
+                        if ( > )
+                        end
+                        else begin
+                    
+                    end
+                end
+                
+                ARREDONDA: begin
+                
+                
+                end
+                
+                VERIFICA: begin
+                
+                end
+                
+                OUTPUT: begin
+                
+                end
+                
+            endcase
         end    
     end
     
